103b21d60cc80ee4306f3d491bf6dc76b2c1a0be
1bijaysharma <bijay@appycodes.com>
2025-08-12 15:35:49 +0530

    Adding error in wip

--- http-request/src/components/AvailablePlaces.jsx
+++ http-request/src/components/AvailablePlaces.jsx
@@ -1,15 +1,24 @@
 import { useEffect, useState } from "react";
 import Places from "./Places.jsx";
+import Error from "./Error.jsx";
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [isFetching, setIsFetching] = useState(true);
   const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [error, setError] = useState();
 
   useEffect(() => {
-    async function fetchPlaces() {
-      const response = await fetch("http://localhost:3000/places");
-      const resData = await response.json();
-      setAvailablePlaces(resData.places);
+    try {
+      async function fetchPlaces() {
+        setIsFetching(true);
+        const response = await fetch("http://localhost:3000/places");
+        const resData = await response.json();
+        setAvailablePlaces(resData.places);
+      }
+    } catch (error) {
+      setError({ message: error.message || "Could not fetch places" });
     }
+    setIsFetching(false);
 
     fetchPlaces();
   }, []);
@@ -18,6 +27,8 @@ export default function AvailablePlaces({ onSelectPlace }) {
     <Places
       title="Available Places"
       places={availablePlaces}
+      isLoading={isFetching}
+      loadingText="Places is loading...."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
