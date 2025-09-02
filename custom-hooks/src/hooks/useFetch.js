7d0d17232f21d10449df0e8cee53c467d5050267
1bijaysharma <bijay@appycodes.com>
2025-09-02 10:00:40 +0530

    Added custom hook project

---
+++ custom-hooks/src/hooks/useFetch.js
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+
+export function useFetch(fetchFn) {
+  const [isFetching, setIsFetching] = useState(false);
+  const [userPlaces, setUserPlaces] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    async function fetchPlaces() {
+      setIsFetching(true);
+      try {
+        const places = await fetchFn();
+        setUserPlaces(places);
+      } catch (error) {
+        setError({ message: error.message || "Failed to fetch user places." });
+      }
+
+      setIsFetching(false);
+    }
+
+    fetchPlaces();
+  }, [fetchFn]);
+
+  return {
+    isFetching,
+    userPlaces,
+    error,
+  };
+}
