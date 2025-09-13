6497a9e89d6a7c86e2adde81365ab9ffc617cba3
Bijay Sharma <digitalbijay17@gmail.com>
2025-09-13 08:05:05 +0530

    Added form

---
+++ forms/src/components/Login.jsx
@@ -0,0 +1,37 @@
+export default function Login() {
+  function handleSubmit(event){
+    event.preventDefault();
+    // console.log(event);
+    console.log("name", event.target.email.value);
+    console.log("name", event.target.password.value);
+    
+    
+  }
+
+  // function handleFormButton(){
+  //   console.log("butto clicked");
+  // }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Login</h2>
+
+      <div className="control-row">
+        <div className="control no-margin">
+          <label htmlFor="email">Email</label>
+          <input id="email" type="email" name="email" />
+        </div>
+
+        <div className="control no-margin">
+          <label htmlFor="password">Password</label>
+          <input id="password" type="password" name="password" />
+        </div>
+      </div>
+
+      <p className="form-actions">
+        <button className="button button-flat">Reset</button>
+        <button className="button">Login</button>
+      </p>
+    </form>
+  );
+}
