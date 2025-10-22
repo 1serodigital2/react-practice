63d73be054976e37be78067fa52df0da4953f771
1bijaysharma <bijay@appycodes.com>
2025-10-22 12:45:39 +0530

    Created custom input hook to manage login form

--- forms/src/components/StateLogin.jsx
+++ forms/src/components/StateLogin.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Input from "./Input";
 
 import { useInput } from "../hooks/useInput";
@@ -9,85 +10,30 @@ export default function Login() {
     handleInputBlur: handleEmailBlur,
     value: emailValue,
     hasError: emailHasError,
+    resetInput: resetEmailInput,
   } = useInput("", (value) => isEmail(value) && isNotEmpty(value));
   const {
     handleInputChange: handlePasswordChange,
     handleInputBlur: handlePasswordBlur,
     value: passwordValue,
     hasError: passwordHasError,
+    resetInput: resetPasswordInput,
   } = useInput("", (value) => hasMinLength(value, 6) && isNotEmpty(value));
 
-  // const [formOk, setFormOk] = useState(null);
-
-  // function handleInputChange(identifier, value) {
-  //   const validInput = validateInput(identifier, value);
-
-  //   console.log("validate input", validInput);
-
-  //   let error;
-
-  //   if (!validInput) {
-  //     error =
-  //       identifier === "email" && value.length > 4
-  //         ? "Invalid email"
-  //         : identifier === "password" && value.length > 4
-  //         ? "Length must be greater than 6"
-  //         : "";
-  //     setErrorText((prevValue) => ({
-  //       ...prevValue,
-  //       [identifier]: error,
-  //     }));
-  //   } else {
-  //     setErrorText((prevValue) => ({
-  //       ...prevValue,
-  //       [identifier]: "",
-  //     }));
-  //   }
-
-  //   setEnteredValues((prevValue) => ({
-  //     ...prevValue,
-  //     [identifier]: value,
-  //   }));
-
-  //   console.log("error text", errorText);
-  // }
-
-  // function validateInput(identifier, inputValue) {
-  //   if (inputValue == "") return false;
-
-  //   if (identifier === "email" && inputValue.includes("@")) return true;
-  //   if (identifier === "password" && inputValue.length > 6) return true;
-
-  //   return false;
-  // }
-
-  // function handleInputBlur(identifier, inputValue) {
-  //   setEnteredValues((prevValue) => ({
-  //     ...prevValue,
-  //     [identifier]: inputValue,
-  //   }));
-
-  //   setEnteredValues((prevValue) => ({
-  //     ...prevValue,
-  //     [identifier]: inputValue,
-  //   }));
-
-  //   console.log(" handleINputBlur", enteredValues);
-  // }
+  const [formOk, setFormOk] = useState(null);
 
   function handleSubmit(event) {
     event.preventDefault();
-    if (errorText.email == "" && errorText.password == "") {
-      setFormOk(true);
-    } else {
+    if (emailHasError || passwordHasError) {
       setFormOk(false);
+    } else {
+      setFormOk(true);
     }
-    console.log("FOrm", formOk);
   }
 
   function clearForm() {
-    setEnteredValues({ email: "", password: "" });
-    console.log("clear form", enteredValues);
+    resetEmailInput();
+    resetPasswordInput();
   }
 
   return (
@@ -102,7 +48,9 @@ export default function Login() {
             onChange={handleEmailChange}
             onBlur={handleEmailBlur}
             inputValue={emailValue}
-            error={emailHasError ? "Invalid email" : ""}
+            error={
+              emailHasError && emailValue.length > 10 && "Invalid email address"
+            }
           />
         </div>
 
@@ -114,9 +62,9 @@ export default function Login() {
             onBlur={handlePasswordBlur}
             inputValue={passwordValue}
             error={
-              passwordHasError
-                ? "Password must be at least 6 characters long"
-                : ""
+              passwordHasError &&
+              passwordValue.length > 3 &&
+              "Password must be at least 6 characters long"
             }
           />
         </div>
@@ -132,11 +80,11 @@ export default function Login() {
         </button>
         <button className="button">Login</button>
       </p>
-      {/* {formOk == false && (
+      {formOk == false && (
         <div className="control-error">
           <p>Please fix invalid input</p>
         </div>
-      )} */}
+      )}
     </form>
   );
 }
--- forms/src/hooks/useInput.js
+++ forms/src/hooks/useInput.js
@@ -3,27 +3,29 @@ import { validateEmail, validatePassword } from "../util/validation";
 
 export function useInput(defaultValue, validationFn) {
   const [enteredValue, setEnteredValue] = useState(defaultValue);
-  const [errorText, setErrorText] = useState("");
 
   const isValidInput = validationFn(enteredValue);
 
   function handleInputChange(event) {
     setEnteredValue(event.target.value);
 
-    console.log("error text", errorText);
     console.log("enteredValue ", enteredValue);
   }
 
-  function handleInputBlur(inputValue) {
-    setEnteredValue(inputValue);
+  function handleInputBlur(event) {
+    setEnteredValue(event.target.value);
     console.log(" handleInputBlur", enteredValue);
+  }
+
+  function handleResetInput() {
+    setEnteredValue("");
   }
 
   return {
     value: enteredValue,
     handleInputBlur,
     handleInputChange,
-    errorText,
-    hasError: !isValidInput,
+    hasError: !isValidInput && enteredValue.trim() !== "",
+    resetInput: handleResetInput,
   };
 }
