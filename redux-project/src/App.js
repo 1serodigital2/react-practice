030a1bb199206d916785858af06d2d34eab3f278
1bijaysharma <bijay@appycodes.com>
2025-12-23 10:42:24 +0530

    Login form using redux

--- redux-project/src/App.js
+++ redux-project/src/App.js
@@ -1,3 +1,5 @@
+import { useSelector } from "react-redux";
+
 import { Fragment } from "react/jsx-runtime";
 
 import Counter from "./components/Counter";
@@ -5,10 +7,12 @@ import Header from "./components/Header";
 import Auth from "./components/Auth";
 
 function App() {
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+
   return (
     <Fragment>
       <Header />
-      <Auth />
+      {!isAuthenticated && <Auth />}
       <Counter />
     </Fragment>
   );
--- redux-project/src/components/Auth.js
+++ redux-project/src/components/Auth.js
@@ -1,17 +1,33 @@
-import classes from './Auth.module.css';
+import { useDispatch } from "react-redux";
+
+import classes from "./Auth.module.css";
+import { authAction } from "../store";
 
 const Auth = () => {
+  const dispatch = useDispatch();
+  const loginAction = (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+
+    const email = formData.get("email");
+    const password = formData.get("password");
+
+    if (email && password) {
+      dispatch(authAction.login());
+    }
+  };
+
   return (
     <main className={classes.auth}>
       <section>
-        <form>
+        <form onSubmit={loginAction}>
           <div className={classes.control}>
-            <label htmlFor='email'>Email</label>
-            <input type='email' id='email' />
+            <label htmlFor="email">Email</label>
+            <input type="email" id="email" name="email" />
           </div>
           <div className={classes.control}>
-            <label htmlFor='password'>Password</label>
-            <input type='password' id='password' />
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password" name="password" />
           </div>
           <button>Login</button>
         </form>
