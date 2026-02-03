02ece340295076a0a6a90cd48d1f080220fb11dd
1bijaysharma <bijay@appycodes.com>
2026-02-03 11:29:52 +0530

    Started react router

---
+++ reactRouter/src/pages/Error.js
@@ -0,0 +1,14 @@
+import Header from "../components/Header";
+
+const ErrorPage = () => {
+  return (
+    <>
+      <Header />
+      <main>
+        <h1>Error page</h1>
+        <p>Could not find this page</p>
+      </main>
+    </>
+  );
+};
+export default ErrorPage;
