e7114bb5ab8e0c36281d1f166279a171645970dc
1bijaysharma <bijay.sharma@creoate.com>
2026-03-18 11:02:25 +0530

    Completed react router practice building events listing page and its details

--- reactRouterPractice/frontend/src/pages/EventDetail.js
+++ reactRouterPractice/frontend/src/pages/EventDetail.js
@@ -1,28 +1,64 @@
-import { redirect, useRouteLoaderData } from "react-router-dom";
+import { Await, redirect, useRouteLoaderData } from "react-router-dom";
 import EventItem from "../components/EventItem";
+import { Suspense } from "react";
+import EventsList from "../components/EventsList";
 
 const EventDetailPage = () => {
-  const data = useRouteLoaderData("event-detail");
-  const event = data.event;
+  const { event, events } = useRouteLoaderData("event-detail");
 
   return (
     <>
-      <EventItem event={event} />
+      <Suspense
+        fallback={
+          <p style={{ textAlign: "center" }}>Loading event detail...</p>
+        }
+      >
+        <Await resolve={event}>
+          {(loadedEventDetail) => <EventItem event={loadedEventDetail} />}
+        </Await>
+      </Suspense>
+      <Suspense
+        fallback={<p style={{ textAlign: "center" }}>Loading all events...</p>}
+      >
+        <Await resolve={events}>
+          {(loadedEvents) => <EventsList events={loadedEvents} />}
+        </Await>
+      </Suspense>
     </>
   );
 };
 
 export default EventDetailPage;
 
-export const loader = async ({ request, params }) => {
-  const response = await fetch(
-    `http://localhost:8080/events/${params.eventId}`,
-  );
+export const loadEvents = async () => {
+  const response = await fetch(`http://localhost:8080/events`);
+
+  if (!response.ok) {
+    throw new Response("Could not fetch events", { status: response.status });
+  } else {
+    const resData = await response.json();
+    console.log("loadEvents", resData);
+    return resData.events;
+  }
+};
+
+export const loadEvent = async (id) => {
+  const response = await fetch(`http://localhost:8080/events/${id}`);
   if (!response.ok) {
     throw new Response("Unable to get event", { status: response.status });
   } else {
-    return response;
+    const resData = await response.json();
+
+    return resData.event;
   }
+};
+
+export const loader = async ({ request, params }) => {
+  const eventId = params.eventId;
+  return {
+    event: await loadEvent(eventId),
+    events: loadEvents(),
+  };
 };
 
 export const deleteEvent = async ({ params, request }) => {
--- reactRouterPractice/frontend/src/pages/Home.js
+++ reactRouterPractice/frontend/src/pages/Home.js
@@ -1,4 +1,10 @@
+import PageContent from "../components/PageContent";
+
 const HomePage = () => {
-  return <h1>HomePage</h1>;
+  return (
+    <PageContent title="Welcome">
+      <p>Greetings to the homepage</p>
+    </PageContent>
+  );
 };
 export default HomePage;
